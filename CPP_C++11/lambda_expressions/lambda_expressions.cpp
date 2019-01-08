172e0026318229054f4a8342a9550a25f62ffce3
812310486@qq.com <812310486@qq.com>
2019-01-09 01:07:51 +0800

    lambda表达式
    lambda表达式

--- CPP_C++11/lambda_expressions/lambda_expressions.cpp
+++ CPP_C++11/lambda_expressions/lambda_expressions.cpp
@@ -1,5 +1,7 @@
 #include <string>
 #include <iostream>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
 // LAMBDA FUNCTION
@@ -25,20 +27,138 @@ using namespace std;
   lambda是能在函数和表达式内部定义的函数式定义
   lambda可被当做inline函数使用
   最小lambda函数没有参数并简单的做一些事情
+  lambda表达式类型是一个匿名函数对象
 */
 
+// 简单应用
 namespace SIMPLE_LAMBDA
 {
   auto I = [] {
       cout << "hello world!" << endl;
   };
 
+  int id = 0;
+  auto F = [] {
+    cout << "F()::id\t" << id << endl;
+    ++id;
+  };
+
+  auto FF = []() mutable {
+    cout << "FF()::id\t" << id << endl;
+    ++id;
+  };
+
   void TEST(){
     I();
+    F();
+    F();
+    FF();
+    FF();
+    cout << "SIMPLE_LAMBDA::id\t" << id << endl;
+  }
+}
+
+// 函数中应用
+void FUNCTION_LAMBDA()
+{
+  int id = 0;
+  // 有可选项时()必须要添加,否则存在编译错误
+  // 对象以传值方式传入，但是在lambda定义的函数对象中通过mutable可以访问传入值
+  auto f = [id] () mutable {
+    cout << "FUNCTION_LAMBDA value :: id\t" << id << endl;
+    ++id;
+  };
+  id = 42;
+
+  auto ff = [&id] {
+    cout << "FUNCTION_LAMBDA reference :: id\t" << id << endl;
+    ++id;
+  };
+
+  f();
+  f();
+  f();
+  cout << "out value id \t" << id << endl;
+  ff();
+  ff();
+  ff();
+  cout << "out reference id \t" << id << endl;
+}
+
+// 标准库应用
+namespace STL_LAMBDA 
+{
+  class Compare {
+  public:
+    Compare() = default;
+    Compare(int pre, int post) : _m(pre), _n(post){
+    }
+
+    // 大小比较
+    bool operator()(int a, int b) {
+      return a < b;
+    }
+
+    // 范围比较
+    bool operator()(int a) {
+      return (a > _m) && (a < _n);
+    }
+
+  private:
+    int _m;
+    int _n;
+  };
+
+  bool compare(int a, int b) {
+    return a < b;
+  }
+
+  void TEST()
+  {
+    vector<int> vec{ 3, 2, 5, 7, 3, 2 };
+    vector<int> ivec(vec);
+    vector<int> iivec(vec);
+    vector<int> iiivec(vec);
+    vector<int> vvec(vec);
+
+    for (auto  i : vec) { cout << i << ' '; }
+    cout << endl;
+
+    // first way
+    sort(vec.begin(), vec.end(), compare);
+    for (auto i : vec) { cout << i << ' '; }
+    cout << endl;
+
+    // second way
+    sort(ivec.begin(), ivec.end(), Compare());
+    for (auto i : ivec) { cout << i << ' '; }
+    cout << endl;
+
+    // third way
+    sort(iivec.begin(), iivec.end(), [](int a, int b)->bool {return a < b; });
+    for (auto i : iivec) { cout << i << ' '; }
+    cout << endl;
+
+    // erase some value
+    // 序列式容器删除指定范围的数值
+    // string 删除指定数值的字符
+    int x = 4;
+    int y = 6;
+    iiivec.erase(remove_if(iiivec.begin(), iiivec.end(), [x, y](int a) {return (a > x) && (a < y); }),
+                 iiivec.end());
+    for (auto i : iiivec) { cout << i << ' '; }
+    cout << endl;
+
+    iiivec.erase(remove_if(iiivec.begin(), iiivec.end(), Compare(2, 5)), iiivec.end());
+    for (auto i : iiivec) { cout << i << ' '; }
+    cout << endl;
+
   }
 }
 
 int main()
 {
   SIMPLE_LAMBDA::TEST();
+  FUNCTION_LAMBDA();
+  STL_LAMBDA::TEST();
 }
